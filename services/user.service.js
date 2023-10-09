5f703fb85d4e635f62c501c6091651d5c69ec6d3
Hendar <hendarsundana@gmail.com>
2023-10-10 06:05:48 +0700

    add bcrypt

--- services/user.service.js
+++ services/user.service.js
@@ -1,11 +1,13 @@
 const { user } = require('../models');
+const bcrypt = require('bcrypt');
 
 class UserService {
     async store(payload) {
         const date = new Date();
         const { name, email, password } = payload;
+        const encript = await bcrypt.hash(password, 10);
         // Simpan data pengguna ke database
-        const data = await user.create({ name, email, password, createdAt: date, updatedAt: date });
+        const data = await user.create({ name, email, password: encript, createdAt: date, updatedAt: date });
         return data;
     }
 }
